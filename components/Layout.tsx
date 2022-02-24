62ec8fab15150dbf7663d3cc46e0330435a07409
Ryan-Sheehan <sheehan.w.ryan@gmail.com>
2022-02-23 23:48:40 -0500

    :bug: typescript error
    Need prop interfaces

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,15 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import sanity from '../lib/sanity';
+import { sanity } from '../lib/sanity';
 import { observer } from 'mobx-react-lite';
-import { useMobxStores } from '../stores/store';
+import { useStore } from '../stores/store';
 import { motion } from 'framer-motion';
 import { getRandomColor } from '../utils/getRandomColor';
 import css from 'styled-jsx/css';
+import { ReactChild, Key } from 'react';
 
 const { className, styles } = css.resolve`
   div {
-    padding: calc(2rem - ${DOT_HEIGHT}px) calc(2rem - ${DOT_WIDTH}px);
-    display: grid;
-    grid-template-rows: ${DOT_HEIGHT}px ${FIXED_ROW_HEIGHT} ${DOT_HEIGHT}px auto ${DOT_HEIGHT}px ${FIXED_ROW_HEIGHT} ${DOT_HEIGHT}px;
-    grid-template-columns:
-      ${DOT_WIDTH}px minmax(0, 1fr) ${DOT_WIDTH}px minmax(0, 1fr) ${DOT_WIDTH}px
-      minmax(0, 1fr) ${DOT_WIDTH}px minmax(0, 1fr) ${DOT_WIDTH}px;
-
     width: 100%;
     height: 100%;
   }
@@ -23,10 +17,7 @@ const { className, styles } = css.resolve`
     div {
       padding: 0px;
       display: grid;
-      grid-template-rows: ${MOBILE_DOT_HEIGHT}px ${MOBILE_FIXED_ROW_HEIGHT} ${MOBILE_DOT_HEIGHT}px auto ${MOBILE_DOT_HEIGHT}px ${MOBILE_FIXED_ROW_HEIGHT} ${MOBILE_DOT_HEIGHT}px;
-      grid-template-columns:
-        ${MOBILE_DOT_WIDTH}px calc(100% - ${MOBILE_DOT_WIDTH * 2}px)
-        ${MOBILE_DOT_WIDTH}px;
+
       grid-column-gap: 0;
     }
   }
@@ -41,21 +32,23 @@ const variants = {
   },
 };
 
-const Layout = observer(({ children, pageInfo, key }) => {
-  return (
-    <motion.div
-      key={key}
-      transition={{ duration: 2 }}
-      initial={'out'}
-      animate={'in'}
-      exit={'out'}
-      variants={variants}
-      className={className}
-    >
-      {children}
-      {styles}
-    </motion.div>
-  );
-});
+const Layout = observer(
+  ({ children, key }: { children: ReactChild; key: Key }) => {
+    return (
+      <motion.div
+        key={key}
+        transition={{ duration: 2 }}
+        initial={'out'}
+        animate={'in'}
+        exit={'out'}
+        variants={variants}
+        className={className}
+      >
+        {children}
+        {styles}
+      </motion.div>
+    );
+  },
+);
 
 export default Layout;
