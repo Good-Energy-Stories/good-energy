22668081549c1bc910f2b0e0fef3365623b7e431
Ryan-Sheehan <sheehan.w.ryan@gmail.com>
2022-04-18 20:03:35 -0400

    portable text fixes

--- components/PortableTextSerializer.tsx
+++ components/PortableTextSerializer.tsx
@@ -4,16 +4,19 @@ const PortableTextSerializer: Partial<PortableTextReactComponents> = {
   block: {
     normal: ({ children }) => <p className="body">{children}</p>,
   },
+  listItem: {
+    bullet: ({ children }) => <li className="body">{children}</li>,
+  },
   marks: {
     strong: ({ children }) => <span className="body-bold">{children}</span>,
     link: ({ value, children }) => {
-      const target = (value?.href || '').startsWith('http')
+      const target = (value?.url || '').startsWith('http')
         ? '_blank'
         : undefined;
       return (
         <a
           className="body-link"
-          href={value?.href}
+          href={value?.url}
           target={target}
           rel={target === '_blank' && 'noindex nofollow'}
         >
