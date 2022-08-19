951281774fae72b75e0bfae53e64e1edf9893cc6
Ryan-Sheehan <sheehan.w.ryan@gmail.com>
2022-08-19 18:50:46 -0400

    better nav sizing

--- components/SecondaryNavMenu/SecondaryNavMenu.tsx
+++ components/SecondaryNavMenu/SecondaryNavMenu.tsx
@@ -1,16 +1,13 @@
-import HamburgerIcon from '../../public/small-hamburger.svg';
-
-import SearchIcon from '../../public/small-search.svg';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../../stores/store';
-import Link from 'next/link';
-import { useState } from 'react';
+
 import css from 'styled-jsx/css';
 import { motion } from 'framer-motion';
 
 import { FRAMER_TRANSITION_EASEOUT } from '../../lib/framer/framer-animations';
-import DropdownMenu from './DropdownMenu';
 import { NavLogo } from '..';
+import SecondaryNavButtons from './SecondaryNavButtons';
+import SecondaryMenuTab from './SecondaryMenuTab';
 
 function getStyles(
   includeNavItems: boolean,
@@ -43,68 +40,6 @@ function getStyles(
   `;
 }
 
-const MenuTab = ({ label, firstArticle, articles, color }) => {
-  const [expanded, setExpanded] = useState(false);
-  const [offset, setOffset] = useState(0);
-
-  return (
-    <>
-      <div
-        className="tab"
-        onMouseEnter={(e) => {
-          setOffset(e.clientX);
-          setExpanded(true);
-        }}
-        onMouseLeave={(e) => {
-          setExpanded(false);
-        }}
-      >
-        <div className="tab-inner">
-          <motion.div whileHover={{ opacity: 0.6 }}>
-            <Link href={`/playbook/${firstArticle?.slug}`}>
-              <a>
-                <div className="playbook-toc-nav-link-small">{label}</div>
-              </a>
-            </Link>
-          </motion.div>
-        </div>
-        {articles?.length > 1 && (
-          <DropdownMenu
-            expanded={expanded}
-            offset={offset}
-            articles={articles}
-          />
-        )}
-        <style jsx>{`
-          .tab {
-            position: relative;
-          }
-          .tab-inner {
-            height: ${SECONDARY_MENU_HEIGHT}px;
-            display: flex;
-            align-items: center;
-            text-align: center;
-          }
-          .playbook-toc-nav-link-small {
-            color: ${color};
-            text-transform: uppercase;
-            cursor: pointer;
-          }
-          @media only screen and (max-width: 1080px) {
-            .tab {
-              display: none;
-            }
-          }
-          @media only screen and (max-width: 768px) {
-            .right {
-            }
-          }
-        `}</style>
-      </div>
-    </>
-  );
-};
-
 export const SECONDARY_MENU_HEIGHT = 40;
 
 const variants = {
@@ -119,43 +54,14 @@ const variants = {
   closed: { height: 0, overflow: 'hidden' },
 };
 
-const NavButtons = observer(() => {
-  const store = useStore();
-  const {
-    uiStore: { openNavOverlay },
-  } = store;
-  return (
-    <div>
-      <button onClick={() => openNavOverlay()}>
-        <motion.div whileHover={{ opacity: 0.6 }} whileTap={{ scale: 0.95 }}>
-          <HamburgerIcon fill={'#000'} />
-        </motion.div>
-      </button>
-      <Link href={'/playbook/search'}>
-        <a style={{ height: 30 }}>
-          <motion.div whileHover={{ opacity: 0.6 }} whileTap={{ scale: 0.95 }}>
-            <SearchIcon fill={'#000'} />
-          </motion.div>
-        </a>
-      </Link>
-      <style jsx>{`
-        button {
-          padding: 0;
-          margin-right: 0.625rem;
-        }
-        div {
-          padding-left: 0.625rem;
-          display: flex;
-          align-items: center;
-        }
-        @media only screen and (max-width: 768px) {
-          .right {
-          }
-        }
-      `}</style>
-    </div>
-  );
-});
+interface SecondaryNavMenu {
+  includeNavItems?: boolean;
+  top?: number;
+  color?: string;
+  backgroundColor?: string;
+  position: string;
+  initial?: string;
+}
 
 const SecondaryNavMenu = observer(
   ({
@@ -165,14 +71,7 @@ const SecondaryNavMenu = observer(
     backgroundColor = 'var(--blueFive)',
     position,
     initial,
-  }: {
-    includeNavItems?: boolean;
-    top?: number;
-    color?: string;
-    backgroundColor?: string;
-    position: string;
-    initial?: string;
-  }) => {
+  }: SecondaryNavMenu) => {
     const { className, styles } = getStyles(
       includeNavItems,
       top,
@@ -186,31 +85,29 @@ const SecondaryNavMenu = observer(
       dataStore: { playbookSections },
     } = store;
     return (
-      <>
-        <motion.div
-          style={{ marginTop: -2 }}
-          transition={FRAMER_TRANSITION_EASEOUT}
-          initial={initial}
-          animate={playbookSecondaryNavOpen ? 'open' : 'closed'}
-          variants={variants}
-          className={className}
-        >
-          {includeNavItems && <NavButtons />}
-          {playbookSections.map((s) => {
-            return (
-              <MenuTab
-                key={s.label}
-                label={s.label}
-                firstArticle={s.firstArticle}
-                articles={s.articles}
-                color={color}
-              />
-            );
-          })}
-          {includeNavItems && <NavLogo height={SECONDARY_MENU_HEIGHT} />}
-        </motion.div>
+      <motion.div
+        style={{ marginTop: -2 }}
+        transition={FRAMER_TRANSITION_EASEOUT}
+        initial={initial}
+        animate={playbookSecondaryNavOpen ? 'open' : 'closed'}
+        variants={variants}
+        className={className}
+      >
+        {includeNavItems && <SecondaryNavButtons />}
+        {playbookSections.map((s) => {
+          return (
+            <SecondaryMenuTab
+              key={s.label}
+              label={s.label}
+              firstArticle={s.firstArticle}
+              articles={s.articles}
+              color={color}
+            />
+          );
+        })}
+        {includeNavItems && <NavLogo height={SECONDARY_MENU_HEIGHT} />}
         {styles}
-      </>
+      </motion.div>
     );
   },
 );
