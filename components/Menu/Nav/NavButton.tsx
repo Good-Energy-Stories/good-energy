92c8c0a02d5003b03393cfc02cbd5966d614ae14
Ryan-Sheehan <sheehan.w.ryan@gmail.com>
2022-11-16 12:56:01 -0500

    more mobile nav cleanup

--- components/Menu/Nav/NavButton.tsx
+++ components/Menu/Nav/NavButton.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import styles from './NavButton.module.css';
 import classnames from 'classnames';
 import { motion } from 'framer-motion';
-import { useStore } from '../../../stores/store';
 import { observer } from 'mobx-react-lite';
 import { useUIStore } from '../../../providers/RootStoreProvider';
 const cx = classnames.bind(styles);
@@ -17,17 +16,22 @@ const NavButton = observer(() => {
       }}
       className={cx('label-medium', styles.button)}
     >
-      {playbookNavOverlayOpen ? 'Hide Contents' : 'Show Contents'}
+      <label className={styles.label}>
+        {playbookNavOverlayOpen ? 'Hide Contents' : 'Show Contents'}
+      </label>
       <motion.svg
-        animate={{ rotate: playbookNavOverlayOpen ? 90 : -90 }}
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
+        animate={{ rotate: playbookNavOverlayOpen ? 0 : -180 }}
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         className={styles.icon}
       >
-        <path d="M7 9.49976L12 14.4998L17 9.49976H7Z" />
+        <path
+          d="M13.0871 6.95137L11.9121 5.77637L6.91211 10.7764L11.9121 15.7764L13.0871 14.6014L9.27044 10.7764L13.0871 6.95137Z"
+          fill="#2FBDF4"
+        />
       </motion.svg>
     </button>
   );
--- components/Menu/PlaybookNav/CloseButton.tsx
+++ components/Menu/PlaybookNav/CloseButton.tsx
@@ -12,10 +12,23 @@ const CloseButton = () => {
   return (
     <>
       <button
-        className={cx('label-small', styles.button)}
+        className={cx('label-medium', styles.button)}
         onClick={handleClick}
       >
-        {'← Hide Contents'}
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.0003 3.33301L11.1753 4.50801L6.52533 9.16634H16.667V10.833H6.52533L11.1753 15.4913L10.0003 16.6663L3.33366 9.99967L10.0003 3.33301Z"
+            fill="#2FBDF4"
+          />
+        </svg>
+
+        <label className={styles.label}>Hide Contents</label>
       </button>
     </>
   );
