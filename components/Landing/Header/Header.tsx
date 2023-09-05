16aff6e2135256592d33c0ccfbb102a3adcdfa13
Joey Yang <joey@thirdslant.com>
2023-09-05 15:04:06 -0700

    feat: add video to homepage

--- components/Landing/Header/Header.tsx
+++ components/Landing/Header/Header.tsx
@@ -25,7 +25,7 @@ const Header = ({
         />
       )}
       <div className={styles.textContainer}>
-        <div className={cx('h1-xl', styles.title)}>{title}</div>
+        <div className={cx(/*'h1-xl',*/ styles.title)}>{title}</div>
         <div className={cx('h3-sentence', styles.subtitle)}>{subtitle}</div>
         <div className={styles.buttonContainer}>
           <CTAButton
@@ -40,7 +40,7 @@ const Header = ({
           <CTAButton
             className={styles.button}
             data={{
-              label: 'Make A Donation',
+              label: 'Support Us',
               link:
                 donateLink ??
                 'https://creativevisions.networkforgood.com/projects/54417-creative-visions-fiscal-sponsorship-good-energy',
@@ -50,6 +50,29 @@ const Header = ({
             }}
           />
         </div>
+      </div>
+      <div className={styles.videoContainer}>
+        <div
+          style={{
+            padding: '56.25% 0 0 0',
+            position: 'relative',
+          }}
+        >
+          <iframe
+            src="https://player.vimeo.com/video/802831875?h=9f70d673ed&title=0&byline=0&portrait=0"
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
       </div>
     </div>
   );
